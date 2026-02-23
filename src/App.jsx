9a09dc338cc06f0603e8009da70df67b2aa7668d
Andrew Nhangud <Andrewnhangud3785@gmail.com>
2026-02-23 17:43:28 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Narbar } from "./components/Navbar/Navbar";
+import "./styles/global.css";
+
+function App() {
+  return (
+    <>
+      <Narbar />
+    </>
+  );
+}
+
+export default App;
