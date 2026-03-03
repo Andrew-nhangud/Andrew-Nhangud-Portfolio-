d9b419295f5b2b85bf07cf510a62bbf7d66f5160
Andrew Nhangud <Andrewnhangud3785@gmail.com>
2026-03-03 18:22:48 +0200

    updated spelling error and add className

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { Narbar } from "./components/Navbar/Navbar";
+import { Navbar } from "./components/Navbar/Navbar";
 import "./styles/global.css";
 
 function App() {
   return (
-    <>
+    <div className="container">
       <Navbar />
-    </>
+    </div>
   );
 }
 
